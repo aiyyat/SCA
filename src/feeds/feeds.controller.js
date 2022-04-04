0fdac96e225f985a4b2d49a193c322efb07ea495
moversly <moversly@gmail.com>
2022-04-05 08:05:57 +1000

    misc changes to endpoint

--- src/feeds/feeds.controller.js
+++ src/feeds/feeds.controller.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const { getFirstNRSSFeed } = require('../utilities/rss-parser');
 const rss_url = "https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss"
 
-router.get("/all", async (request, response) => {
+router.get("/", async (request, response) => {
     const result = await getFirstNRSSFeed(rss_url);
     response.status(200).send({ result })
 });
 
 router.get("/sort", async (request, response) => {
-    const sort = request.query.order || `des`;
-    const result = await getFirstNRSSFeed(rss_url,sort);
-    response.status(200).send({ result })
+    const feeds = await getFirstNRSSFeed(rss_url, request.query.order || `des`);
+    response.status(200).send({ feeds })
 });
 module.exports = router;
